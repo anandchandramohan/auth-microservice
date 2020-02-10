6a4c50393eae1f3021c38f51eaa5981f2d7f4729
Anand Chandramohan <anand.chandramohan@marketinline.com>
2020-02-10 21:10:28 +0530

    Intial project structure
    - Basic packages
    - REST stub

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// defining the Express app
+const app = express();
+// adding Helmet to enhance your API's security
+app.use(helmet());
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+// enabling CORS for all requests
+//app.use(cors());
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+// defining an endpoint to return all ads
+const ads = [
+    {title: 'Hello, world (again)!'}
+];
+/*app.get('/', (req, res) => {
+    res.send(ads);
+});*/
+// Import routes
+let apiRoutes = require("./routes/api-routes")
+// Use Api routes in the App
+app.use('/api', apiRoutes)
+// starting the server
+app.listen(3001, () => {
+    console.log('listening on port 3001');
+});
