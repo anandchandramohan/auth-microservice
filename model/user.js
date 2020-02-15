6527f7f9ebcc129c748858ea2b0a6bd12dc366f8
Manimaran Selvan <mselvan@belyf.com>
2020-02-15 10:44:43 +0530

    Changing the bcrypt lib source
    The previous bcrypt dependency was an unmaintained source.

--- model/user.js
+++ model/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcryptjs');
 const db = require("../db/connection.js");
 const connection = db.getMongooseConnection();
 var fields = {
